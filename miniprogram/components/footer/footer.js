9b964f7b94a0acfe6d231c5a837b09e7bffa3a79
Wenda Gu <wendagu@usc.edu>
2023-12-14 23:19:02 -0800

    add component footer

---
+++ miniprogram/components/footer/footer.js
@@ -0,0 +1,31 @@
+// components/footer/footer.js
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    content: {
+      type: String,
+      value: 'Default Title'
+    },
+    d: {
+      type: Number,
+      value: 3,
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
